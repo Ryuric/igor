23e1b267746f5d88bd1c48527e37959bf8da32ed
Ryuric <rikardof@gmail.com>
2021-01-31 21:13:02 -0300

    first Igor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,21 @@ function App() {
       </header>
     </div>
   );
+} */
+
+const alanKey = '4e4ed9007e922d13c3d02c19a3ca08282e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command}) => {
+        if (command === 'testCommand') {
+          alert('This code was executed');
+        }
+      }
+    })
+  }, [])
 }
 
 export default App;
